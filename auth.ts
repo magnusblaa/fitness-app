f3f8f88d7daaf5f3f7c7066e1d48dc72fcab1afd
magnus <202006492@post.au.dk>
2023-11-13 09:20:30 +0100

    testing

--- auth.ts
+++ auth.ts
@@ -3,11 +3,16 @@ import Credentials from 'next-auth/providers/credentials';
 import CredentialsProvider from "next-auth/providers/credentials";
 import type { NextAuthConfig } from "next-auth"
 import GitHub from "next-auth/providers/github"
+import { NextResponse } from "next/server";
 
 export const config ={
+  session: {
+    strategy: "jwt"
+  },
   providers: [
     GitHub,
     Credentials({
+    
     name: "Credentials",
     credentials: {
       email: { label: "Email", type: "text" },
@@ -29,13 +34,32 @@ export const config ={
       const data = await res.json();
       console.log(data);
       return data;
-    }
+    },
+    
   })
   ],
+  
   callbacks: {
-    authorized({ request, auth }) {
-      return !!auth
-      // return false;
+    async authorized({ request, auth }) {
+      // const url = request.nextUrl
+      const isLoggedIn = !!auth;
+      if (isLoggedIn){
+        return true;
+      } else{
+        return false;
+      }
+      
+      // if(request.method === "POST") {
+      //   const { authToken } = (await request.json()) ?? {}
+      //   // If the request has a valid auth token, it is authorized
+      //   const valid = await validateAuthToken(authToken)
+      //   if(valid) return true
+      //   return NextResponse.json("Invalid auth token", { status: 401 })
+      // }
+    
+      // // Logged in users are authenticated, otherwise redirect to login page
+      // return !!auth?.user
+      
     },
     async jwt({token, user}){
       if(user){
